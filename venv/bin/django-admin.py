b86f6cf173d5ec595dc6a86e39b9c93dcb5140a5
Shivang Chopra <shivang.chopra@codenation.co.in>
2019-05-28 14:13:59 +0530

    removed extra files

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/shivangchopra/Desktop/CN/project_management/project_app/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
